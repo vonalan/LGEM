28831306b2388cf84299671c6af5aeecde9f1137
vonalan <kingdom_lee@outlook.com>
2017-06-20 21:12:27 +0800

    deleted:    src/STSM_precise.m 	deleted:    src/STSM_pseudo.m 	deleted:    src/centerNum.m 	deleted:    src/findCenter.m 	deleted:    src/getmatrix01.m 	deleted:    src/label2vector.m 	deleted:    src/preProcesse.m 	deleted:    src/runTest.m 	deleted:    src/run_rbfnn.m 	deleted:    src/test.m 	deleted:    src/testRBFNN.m 	deleted:    src/trainRBFNN.m

--- src/testRBFNN.m
+++
@@ -1,9 +0,0 @@
-function [ output ] = testRBFNN( Xtest, param )
-% compute RBFNN output
-
-t1 = pdist2(Xtest, param.U).^2;
-t2 = exp(bsxfun(@rdivide, t1, (- 2 * (param.V.^2))'));
-output = t2 * param.W;
-
-end
-
