ff31af402c1d61fd44305211497157e30f49e24c
Kingdom Lee <snoe_flying@hotmail.com>
2017-11-24 00:29:26 +0800

    m

--- src/python3/stsm_pseudo.py
+++ src/python3/stsm_pseudo.py
@@ -28,3 +28,25 @@ def calc_stsm(trainX, trainY, W, U, V, Q=0.1):
     STSM = np.mean(STSM, axis = 0)
 
     return STSM
+
+class RadialBaiscFunctionNeuralNetwork(object):
+    def __init__(self, sizes=[]):
+        pass
+    def fit(self):
+        pass
+    def predict(self):
+        pass
+model = RadialBaiscFunctionNeuralNetwork()
+
+
+def calc_stsm_vector(model, trainX, cost_func=None, num_delta=1000, Q=0.1):
+    deltaX = np.random.uniform(-Q, Q, (num_delta, trainX.shape[1]))
+    trainY = model.predict(trainX)
+
+    STSM = np.zeros(trainY.shape)
+    for i in range(num_delta):
+        deltaY = model.predict(trainX + deltaX[i,:])
+        STSM += cost_func(deltaY, trainY)
+    STSM = STSM/num_delta
+    stsm_vector = STSM.mean(axis=0)
+    return stsm_vector
